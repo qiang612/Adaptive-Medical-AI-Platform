6329e87a8bc9318289764a84687267b7b665e0b4
qiang612 <3235358845@qq.com>
2026-03-13 16:35:36 +0800

    3.13 16.35 快速更新

--- backend/app/main.py
+++ backend/app/main.py
@@ -94,8 +94,7 @@ app = FastAPI(
 app.mount("/uploads", StaticFiles(directory=settings.UPLOAD_DIR), name="uploads")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",
-        "http://127.0.0.1:3000","*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
